2b13b4f9dac5f35817fa665bef49a96dd9c662c6
Vaugusto <samlara273@gmail.com>
2025-09-16 15:03:25 -0300

    ✨ HTML form improvement, JS form processing + Form elements now have for, name, id and new classes     - label     - input     - and button + Changed btn to darkTogglebtn + Const saved is now on Variables + Added const crudBtn to select all forms buttons (Create, Read, Update, Delete) + Added Query Selector for all forms separately and unified to clean all values when change form or go home + Added form cleaners code to clear all inputs when change the form + Improved EventListener for easy read + Added form processing v1 to display a error on input when click on button and doesn't match the required inputs

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,10 +1,19 @@
-const btn = document.getElementById('darkToggle'); //Procura o ID 'darkToggle'
+///Variables
+const darkTogglebtn = document.getElementById('darkToggle'); //Procura o ID 'darkToggle'
 const body = document.body; //Seleciona o elemento body
 const cards = document.querySelectorAll('.crud-card');
+const saved = localStorage.getItem('theme');
+const crudBtn = document.querySelectorAll('.crud-btn')
+const createform = document.querySelectorAll('.form-create');
+const readform = document.querySelectorAll('.form-read');
+const updateform = document.querySelectorAll('.form-update');
+const removeform = document.querySelectorAll('.form-remove');
+const generalform = document.querySelectorAll('.form-control');
 
+//Functions
 function updateIcon() { //Função para dar update no icone
     const current = body.getAttribute('data-bs-theme'); //Seleciona o atributo dentro do data-bs-theme
-    btn.textContent = current === 'dark' ? '☀️' : '🌙'; //Define qual será o icone dependendo do atributo dentro do data-bs-theme
+    darkTogglebtn.textContent = current === 'dark' ? '☀️' : '🌙'; //Define qual será o icone dependendo do atributo dentro do data-bs-theme
 }
 function showCard(id){
     cards.forEach(c => c.classList.add('d-none'));
@@ -13,19 +22,148 @@ function showCard(id){
         document.getElementById(id).scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
 }
-document.getElementById('menuHome').addEventListener('click', e => { e.preventDefault(); showCard(null); });
-document.getElementById('menuCreate').addEventListener('click', e => { e.preventDefault(); showCard('cardCreate'); });
-document.getElementById('menuRead').addEventListener('click', e => { e.preventDefault(); showCard('cardRead'); });
-document.getElementById('menuUpdate').addEventListener('click', e => { e.preventDefault(); showCard('cardUpdate'); });
-document.getElementById('menuDelete').addEventListener('click', e => { e.preventDefault(); showCard('cardRemove'); });
-btn.addEventListener('click', () => {
+// EventListener
+document.getElementById('menuHome').addEventListener('click', e => { 
+    e.preventDefault(); 
+    showCard(null); 
+    generalform.forEach(input => {
+        const campo = document.getElementById(input.id); 
+        campo.value = "";
+        campo.classList.remove('is-valid'); 
+        campo.classList.remove('is-invalid');
+    })
+});
+
+document.getElementById('menuCreate').addEventListener('click', e => {
+    e.preventDefault(); 
+    showCard('cardCreate');
+    generalform.forEach(input => {
+        const campo = document.getElementById(input.id);
+        campo.value = "";
+        campo.classList.remove('is-valid'); 
+        campo.classList.remove('is-invalid');
+    })
+});
+
+document.getElementById('menuRead').addEventListener('click', e => { 
+    e.preventDefault(); 
+    showCard('cardRead');
+    generalform.forEach(input => {
+        const campo = document.getElementById(input.id);
+        campo.value = "";
+        campo.classList.remove('is-valid'); 
+        campo.classList.remove('is-invalid');
+    })
+});
+
+document.getElementById('menuUpdate').addEventListener('click', e => { 
+    e.preventDefault(); 
+    showCard('cardUpdate');
+    generalform.forEach(input => {
+        const campo = document.getElementById(input.id);
+        campo.value = "";
+        campo.classList.remove('is-valid'); 
+        campo.classList.remove('is-invalid');
+    })
+});
+
+document.getElementById('menuDelete').addEventListener('click', e => { 
+    e.preventDefault(); 
+    showCard('cardRemove');
+    generalform.forEach(input => {
+        const campo = document.getElementById(input.id);
+        campo.value = "";
+        campo.classList.remove('is-valid'); 
+        campo.classList.remove('is-invalid');
+    })
+});
+
+darkTogglebtn.addEventListener('click', () => {
     const current = body.getAttribute('data-bs-theme'); //Seleciona o atributo dentro do data-bs-theme
     body.setAttribute('data-bs-theme', current === 'dark' ? 'light' : 'dark'); //Configura o atributo, utilizando um if, se current tiver o valor de dark dentro, então ele vai mudar para light, caso não, ou seja caso seja diferente de dark, ele muda o valor para dark
     updateIcon();
     localStorage.setItem('theme', body.getAttribute('data-bs-theme')); //Salva no cache
 });
-
-// ao recarregar o site, pega o dado no cache
-const saved = localStorage.getItem('theme');
+//Tratamento do formulario v1
+crudBtn.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        e.preventDefault();
+        let c = 0;
+        switch (btn.id){
+            case 'btn-create':
+                createform.forEach(input =>{
+                    const campo = document.getElementById(input.id);
+                    if (input.value.trim() === ""){
+                        campo.classList.remove('is-valid')
+                        campo.classList.add('is-invalid')
+                    }else{
+                        campo.classList.remove('is-invalid')
+                        campo.classList.add('is-valid')
+                    }
+                })
+                break;
+            case 'btn-read':
+                c = 0
+                readform.forEach(input =>{
+                    const campo = document.getElementById(input.id);
+                    if (input.value.trim() === "" ){
+                        c++;
+                        if (c == 4){
+                            readform.forEach(input =>{
+                                const campo = document.getElementById(input.id);
+                                campo.classList.remove('is-valid');
+                                campo.classList.add('is-invalid');
+                            })
+                        }else{
+                            campo.classList.add('is-valid');
+                            campo.classList.remove('is-invalid');
+                        }
+                    }else{
+                        campo.classList.remove('is-invalid');
+                        campo.classList.add('is-valid');
+                    }
+                })
+                break;
+            case 'btn-update':
+                updateform.forEach(input =>{
+                    const campo = document.getElementById(input.id);
+                    if (input.value.trim() === ""){
+                        campo.classList.remove('is-valid')
+                        campo.classList.add('is-invalid')
+                    }else{
+                        campo.classList.remove('is-invalid')
+                        campo.classList.add('is-valid')
+                    }
+                })
+                break;
+            case 'btn-remove':
+                c = 0;
+                removeform.forEach(input =>{
+                    const campo = document.getElementById(input.id);
+                    if (input.value.trim() === "" ){
+                        c++
+                        if (c == 4){
+                            removeform.forEach(input =>{
+                                const campo = document.getElementById(input.id);
+                                campo.classList.remove('is-valid');
+                                campo.classList.add('is-invalid');
+                            })
+                        }else{
+                            campo.classList.add('is-valid');
+                            campo.classList.remove('is-invalid');
+                        }
+                    }else{
+                        campo.classList.remove('is-invalid');
+                        campo.classList.add('is-valid');
+                    }
+                })
+                break;
+            default:
+                console.log('something gone wrong!');
+                break;
+        }
+    })
+})
+// Extras
 if (saved) body.setAttribute('data-bs-theme', saved);
 updateIcon();
